b24c0d74cff1e1af3ebecf2069bb6afc3ed441df
simoncerman <mineropka34@gmail.com>
2021-11-13 15:27:02 +0100

    Basic rework to PDO

--- scripts/DbCreate.php
+++ scripts/DbCreate.php
@@ -1,44 +1,31 @@
 <?php
-class DBCreater{
-    public function  (){
-
-    }
-}
-
-function CreateConn()
+class DBCreater
 {
-    $servername = "localhost";
-    $username = "19ia04";
-    $password = "2424383398651425";
-    $dbname = "19ia04";
-
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    // Check connection
-    if (!$conn) 
+    private $pdoConn;
+    public function __construct()
     {
-        die("Connection failed: " . mysqli_connect_error());
+        $this->pdoConn = $this->CreateConn();
+    }
+    private function CreateConn()
+    {
+        /*
+        $servername = "localhost";
+        $username = "19ia04";
+        $password = "2424383398651425";
+        $dbname = "19ia04";
+        */
+        $pdo = new PDO('mysql:host=localhost;dbname=19ia04', '19ia04', '2424383398651425');
+        return $pdo;
+    }
+    function CreateDefault()
+    {
+        $sql = '
+        CREATE TABLE  globalInfo(
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            SiteName VARCHAR(50) NOT NULL,
+            SiteType VARCHAR(30)
+            )';
+        $this->pdoConn->query($sql);
     }
 }
-function CreateDefault()
-{
-    /**
-     * TODO: Rework into PDO!
-     */
-    $sql = '
-    CREATE TABLE  globalInfo(
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        SiteName VARCHAR(50) NOT NULL,
-        SiteType VARCHAR(30)
-        )'
-    $conn = CreateConn()
-    if (mysqli_query($conn, $sql)) {
-        echo "Table MyGuests created successfully";
-    } else {
-        echo "Error creating table: " . mysqli_error($conn);
-    }
-}
-function NewDefaultData()
-{
-    
-}
+$DBCreater = new DBCreater();
--- scripts/SiteAccess.php
+++ scripts/SiteAccess.php
@@ -1,7 +1,7 @@
 <?php
-require 'DbCreate.php';
-if($_POST["type"] == "NameTypeFirstInsert"){
+require(__DIR__ . '\DbCreate.php');
+if ($_POST["type"] == "NameTypeFirstInsert") {
        $webType = $_POST["webType"];
        $webName = $_POST["webName"];
-       
+       $DBCreater->CreateDefault();
 }
